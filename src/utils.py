d5e69c69bcfdc8d520eb5fcffee545c55f415bc4
Nurlan-Aliev <nualiev612@gmail.com>
2025-02-12 14:26:00 +0400

    utils for print and input

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,17 @@
 from time import sleep
+from rich.console import Console
 
 
-def show(info: str):
-    print(info)
-    # sleep(1)
+console = Console()
 
 
-def hear(info: str):
-    return input(info + "\n-> ")
+def show(info: str, style: str = "", second: float = 0.05):
+    for latter in info:
+        console.print(latter, end="", style=style)
+        sleep(second)
+    print()
+
+
+def hear(info: str, style=""):
+    show(info, style=style)
+    return console.input("-> ")
