b5a216320c06b9aa59ed40e5abab415e801e4507
Nurlan-Aliev <nualiev612@gmail.com>
2025-06-08 02:02:40 +0400

    open close room

--- src/game/classes/room.py
+++ src/game/classes/room.py
@@ -1,13 +1,32 @@
+from src.game.classes.base import Character
+from src.utils import show
+
+
 class Room:
-    def __init__(self, name: str, creature):
+    def __init__(self, name: str, creature: Character | None, blocked: bool = False):
         self.name = name
         self.creature = creature
         self.next_rooms: dict[str, Room] = {}
+        self.blocked = blocked
 
     def add_rooms(self, *args):
         self.next_rooms.update(
             {room.name: room for room in args if isinstance(room, Room)}
         )
+
+    def change_room(self, choice):
+
+        if choice not in self.next_rooms.keys():
+            return f"{choice} it is not a room"
+
+        if not self.next_rooms[choice].blocked:
+            return self.next_rooms[choice]
+
+        show("this room is blocked")
+        return self
+
+    def unblocked(self):
+        self.blocked = False
 
     def __str__(self):
         return self.name
